ca6a99d6621318c5dbddfd85c49101a2998e69f8
enoch-strok <enochstrok@outlook.com>
2020-07-02 15:58:42 -0700

    successful code... weather data populating properly

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 const superagent = require('superagent');
-const { request } = require('express');
+const {
+    request
+} = require('express');
 
 //------------Celcius to Farenheit Calc----------//
-function convertToF(celsius) {
-    return celsius * 9/5 + 32;
-};
+// function convertToF(celsius) {
+//     return celsius * 9/5 + 32;
+// };
+
 
 //-------------HOME----------------------//
 
@@ -25,15 +28,16 @@ app.get('/', (request, response) => {
 
 //-------------LOCATION-------------------//
 app.get('/location', (request, response) => {
+    console.log('///////////////////////////////////////////////////////////// NEW SEARCH /////////////////////////////////////////////////////////////////////')
     const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE}&q=${request.query.city}&format=json`;
 
     superagent.get(API)
         .then(data => {
-            console.log(data.body[0], request.query.city,'line32');
+            console.log('////////////////////////// LINE 32 ///////////////////////// Location Data.Body: ', data.body[0], request.query.city);
             let locationData = new Location(data.body[0], request.query.city);
             response.status(200).send(locationData);
         })
-        .catch( () => {
+        .catch(() => {
             response.status(500).send('Something went wrong with your search selection!');
         })
 });
@@ -41,7 +45,7 @@ app.get('/location', (request, response) => {
 function Location(obj, citySearch) {
     this.latitude = obj.lat;
     this.longitude = obj.lon;
-    this.formatted_query = obj.display_name +'           ---           ' + ' Lat: ' + obj.lat + ' Lon: ' + obj.lon;
+    this.formatted_query = obj.display_name + '           ---           ' + ' Lat: ' + obj.lat + ' Lon: ' + obj.lon;
     this.search_query = citySearch;
 }
 
@@ -98,31 +102,31 @@ function Location(obj, citySearch) {
 
 //----------NEW WEATHER------------------------//
 app.get('/weather', (request, response) => {
-    
-    const lat = request.query.latitude;
-    const lon= request.query.longitude;
 
-//     //------Today's Date Generator--------//
-//     const today = new Date();
-//     const this_day = String(today.getDate()).padStart(2, '0');
-//     const this_month = String(today.getMonth() + 1).padStart(2, '0');
-//     const this_year = today.getFullYear();
-//     const start_date = this_year + '-' + this_month + '-' + this_day;
-    
-//     let dateArray = []
-//     let i;
-//     for(i = 0; i < 8; i++) {
-//         let d = new Date();
-//         d.setDate(d.getDate() + i);
-//         let end_day = String(d.getDate()).padStart(2, '0');
-//         let end_month = String(d.getMonth() + 1).padStart(2, '0');
-//         let end_year = d.getFullYear();
-//         let end_date = end_year + '-' + end_month + '-' + end_day;
-//         dateArray.push(end_date);
-        
-//     };
-//     console.log(dateArray);
-    
+    const lat = request.query.latitude;
+    const lon = request.query.longitude;
+
+    //------Today's Date Generator--------//
+    // const today = new Date();
+    // const this_day = String(today.getDate()).padStart(2, '0');
+    // const this_month = String(today.getMonth() + 1).padStart(2, '0');
+    // const this_year = today.getFullYear();
+    // const start_date = this_year + '-' + this_month + '-' + this_day;
+
+    // let dateArray = []
+    // let i;
+    // for(i = 0; i < 8; i++) {
+    //     let d = new Date();
+    //     d.setDate(d.getDate() + i);
+    //     let end_day = String(d.getDate()).padStart(2, '0');
+    //     let end_month = String(d.getMonth() + 1).padStart(2, '0');
+    //     let end_year = d.getFullYear();
+    //     let end_date = end_year + '-' + end_month + '-' + end_day;
+    //     dateArray.push(end_date);
+
+    // };
+    // console.log(dateArray);
+
     //------Future Date Generator----------//
     // const d = new Date();
     // d.setDate(d.getDate() + 1);
@@ -134,24 +138,38 @@ app.get('/weather', (request, response) => {
     // console.log('Today\'s Date: ' + start_date);
     // console.log('+7 Days From Today: ' + end_date);
 
-    
+
     //2020-06-25 date format//
     // const API_Historical = `https://api.weatherbit.io/v2.0/history/daily?&lat=${lat}&lon=${lon}&country=US&start_date=${start_date}&end_date=${end_date}&key=${process.env.WEATHER_API}`   
     // const API_current(WORKING) = `https://api.weatherbit.io/v2.0/current?&lat=${lat}&lon=${lon}&country=US&start_date=${start_date}&end_date=${end_date}&key=${process.env.WEATHER_API}`
     const API = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&country=US&key=${process.env.WEATHER_API}`
+
+    let newWeatherArray = [];
+
     superagent.get(API)
+        // .query(queryObject)
         .then(data => {
+            const weatherArray = data.body.data;
+            // console.log('(///////////////////////////////////////////////// line 145 /////////////////////////////////) WeatherArray Console Log: ', weatherArray);
+            weatherArray.forEach(data => {
+                let specificWeatherData = new Weather(data);
+                newWeatherArray.push(specificWeatherData);
+            });
+            response.status(200).send(newWeatherArray);
+            console.log('//////////////////////// line 150 //////////////////////////////////// newWeatherArray: ', newWeatherArray);
 
-            let weather_description = data.body.data[0];
-            console.log(weather_description);
-
-                
-            } );
-
-        });
+        })
+        .catch(() => {
+            response.status(500).send('Something is wrong with your Weather Data...')
+        })
+})
 
 
+function Weather(dataBody) {
+    this.time = new Date(dataBody.datetime).toDateString();
+    this.forecast = dataBody.weather.description;
 
+};
 
 //---------------OLD RESTAURANTS-----------------------------//
 // $('thing').on('something', () => {})
