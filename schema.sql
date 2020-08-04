86ec39465daad7fa62f2c3afd14cb94cfc86267c
enoch-strok <enochstrok@outlook.com>
2020-08-03 23:32:27 -0700

    movies feature

--- schema.sql
+++ schema.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS cities;
 
-CREATE table users (
+CREATE table cities (
     id SERIAL PRIMARY KEY,
     search_query VARCHAR(255),
     formatted_query VARCHAR(255),
-    latitude VARCHAR(255),
-    longitude VARCHAR(255),
-
+    latitude decimal(10,7),
+    longitude decimal(10,7)
 );
